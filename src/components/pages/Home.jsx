da73f7fd3f809ed4b9d0332f6782061bd372c210
Modearbane <mstreleckis@gmail.com>
2025-04-01 17:24:11 +0300

    updated

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -44,8 +44,8 @@ const Home = () => {
       />
 
       <motion.a
-        href={`${process.env.PUBLIC_URL}/cv.pdf`}
-        download="CV.pdf" 
+        href="https://Modearbane.github.io/Partfolio/cv.pdf"
+        download="CV.pdf"
         className="cv-button"
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
